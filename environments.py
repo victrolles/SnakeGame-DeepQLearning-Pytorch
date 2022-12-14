c07cf56c98dec2e85c05364ff3c3a1fa05607afa
Victor Goudal <vi.goudal@gmail.com>
2022-12-14 13:20:50 +0100

    rework and optimized env

---
+++ environments.py
@@ -0,0 +1,150 @@
+import numpy as np
+from enum import Enum
+from collections import namedtuple
+import time
+
+Size_screen = namedtuple('Size_screen', ['width', 'height'])
+Size_grid = namedtuple('Size_grid', ['width', 'height'])
+Coordinates = namedtuple('Coordinates', ['x', 'y'])
+
+BACKGROUND_COLOR = (0, 102, 0)
+RED = (255, 0, 0)
+PINK = (255, 0, 255)
+DARK_PINK = (102, 0, 102)
+BLUE = (0, 0, 255)
+DARK_BLUE = (0, 0, 153)
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+
+class Environment:
+    def __init__(self, size_grid):
+        self.size_grid = size_grid
+        self.reset()
+
+    def reset(self):
+        self.snake = Snake(self.size_grid)
+        self.apple = Apple(self.snake.snake_coordinates, self.size_grid)
+        self.score = 0
+        self.interation = 0
+
+    def run(self):
+        while True:
+            self.play(1)
+            time.sleep(2)
+
+    def play(self, action):
+        # update or reset variables
+        self.interation += 1
+        reward = 0
+        done = False
+
+        # move snake
+        self.snake.move(action)
+
+        # check if snake is closer to apple
+        print("snake head: ", self.snake.snake_coordinates[0])
+        print("apple: ", self.apple.apple_coordinate)
+        current_dist = np.sqrt((self.snake.snake_coordinates[0].x - self.apple.apple_coordinate.x)**2 + (self.snake.snake_coordinates[0].y - self.apple.apple_coordinate.y)**2)
+        old_dist = np.sqrt((self.snake.snake_coordinates[1].x - self.apple.apple_coordinate.x)**2 + (self.snake.snake_coordinates[1].y - self.apple.apple_coordinate.y)**2) if self.snake.length > 1 else 0
+        if current_dist < old_dist:
+            reward = 1
+        else:
+            reward = -1
+
+        # check if snake eats apple
+        if self.snake.snake_coordinates[0] == self.apple.apple_coordinate:
+            self.snake.grow()
+            self.apple.move()
+            self.score += 1
+            reward = 10
+
+        # check if snake collides with itself or with the wall
+        if self.is_collision():
+            done = True
+            reward = -100
+            return reward, done, self.score
+
+        return reward, done, self.score
+
+    def is_collision(self):
+        # check if snake collides with itself
+        for i in range(3, self.snake.length):
+            if self.snake.snake_coordinates[0] == self.snake.snake_coordinates[i]:
+                return True
+
+        # check if snake collides with the wall
+        if self.snake.snake_coordinates[0].x < 0 or self.snake.snake_coordinates[0].x >= self.size_grid.width:
+            return True
+        if self.snake.snake_coordinates[0].y < 0 or self.snake.snake_coordinates[0].y >= self.size_grid.height:
+            return True
+
+        return False
+
+class Snake:
+    def __init__(self, size_grid):
+        self.size_grid = size_grid
+        self.init()
+
+    def init(self):
+        self.length = 1
+        self.direction = Direction(np.random.randint(1,5))
+        self.snake_coordinates = [Coordinates(np.random.randint(1,self.size_grid.width-1),np.random.randint(1,self.size_grid.height-1))]
+        self.old_tail_coordinate = self.snake_coordinates[-1]
+
+    def move(self, action):
+        # [straight, right, left]
+
+        clock_wise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
+        idx = clock_wise.index(self.direction)
+
+        if np.array_equal(action, [1, 0, 0]):
+            new_dir = clock_wise[idx] # no change
+        elif np.array_equal(action, [0, 1, 0]):
+            next_idx = (idx + 1) % 4
+            new_dir = clock_wise[next_idx] # right turn r -> d -> l -> u
+        else: # [0, 0, 1]
+            next_idx = (idx - 1) % 4
+            new_dir = clock_wise[next_idx] # left turn r -> u -> l -> d
+
+        self.direction = new_dir
+
+        if self.direction == Direction.RIGHT:
+            new_snake_coordinate = Coordinates(self.snake_coordinates[0].x + 1, self.snake_coordinates[0].y)
+        elif self.direction == Direction.LEFT:
+            new_snake_coordinate = Coordinates(self.snake_coordinates[0].x - 1, self.snake_coordinates[0].y)
+        elif self.direction == Direction.UP:
+            new_snake_coordinate = Coordinates(self.snake_coordinates[0].x, self.snake_coordinates[0].y - 1)
+        elif self.direction == Direction.DOWN:
+            new_snake_coordinate = Coordinates(self.snake_coordinates[0].x, self.snake_coordinates[0].y + 1)
+
+        self.old_tail_coordinate = self.snake_coordinates[-1]
+        self.snake_coordinates.insert(0, new_snake_coordinate)
+        self.snake_coordinates.pop()
+
+    def grow(self):
+        self.snake_coordinates.append(self.old_tail_coordinate)
+        self.length += 1
+
+class Apple:
+    def __init__(self, snake_coordinates, size_grid):
+        self.size_grid = size_grid
+        self.apple_coordinate = Coordinates(0, 0)
+        self.snake_coordinates = snake_coordinates
+        self.move()
+
+    def move(self):
+        while True:
+            self.apple_coordinate = Coordinates(np.random.randint(self.size_grid.width), np.random.randint(self.size_grid.height))
+            for snake_coordinate in self.snake_coordinates:
+                if self.apple_coordinate == snake_coordinate:
+                    continue
+            break
+
+if __name__ == '__main__':
+    size_grid = Size_grid(10,8)
+    env = Environment(size_grid)
+    env.run()
--- test.py
+++ test.py
@@ -1,45 +1,4 @@
-import time
-import multiprocessing as mp
+import numpy as np
 
-def counter(num):
-    count = 0
-    while count < num:
-        count+=1
-
-def main():
-    print("nbr of cpu:", mp.cpu_count())
-    print("starting")
-    start = time.perf_counter()
-
-
-    p1 = mp.Process(target=counter, args=(125000000,))
-    p2 = mp.Process(target=counter, args=(125000000,))
-    p3 = mp.Process(target=counter, args=(125000000,))
-    p4 = mp.Process(target=counter, args=(125000000,))
-    p5 = mp.Process(target=counter, args=(125000000,))
-    p6 = mp.Process(target=counter, args=(125000000,))
-    p7 = mp.Process(target=counter, args=(125000000,))
-    p8 = mp.Process(target=counter, args=(125000000,))
-
-    p1.start()
-    p2.start()
-    p3.start()
-    p4.start()
-    p5.start()
-    p6.start()
-    p7.start()
-    p8.start()
-
-    p1.join()
-    p2.join()
-    p3.join()
-    p4.join()
-    p5.join()
-    p6.join()
-    p7.join()
-    p8.join()
-
-    print("finished in:", time.perf_counter()-start, "seconds")
-
-if __name__ == '__main__':
-    main()
+list = [1,2,3,4,5]
+print(list[-1])
